0824dcceb59a435ba3648577f7d3a0561ad2700a
Andrey Kozlov <andrew08552gmail.com>
2023-03-19 14:33:33 +0300

    done task 'Tyry pyry'

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@
 //             `Слово "${word}" не является паллидромом`)
 // }
 // console.log(palindrome("топот"))
+
+// ____________________item_2____________________________
+// function tyry_pyry(){
+//     let numbers = [];
+//     for (let i = 1; i <=100; i++){
+//         if (i % 3 == 0 && i % 5 == 0){
+//             numbers.push("тыры-пыры")
+//         }else if (i % 3 == 0){
+//             numbers.push("тыры")
+//         }else if(i % 5 == 0){
+//             numbers.push("пыры")
+//         }else{
+//             numbers.push(i)
+//         }
+//     }
+//     return numbers
+// }
+// for (let i of tyry_pyry())console.log(i);
